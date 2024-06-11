9caaa3cabd34e1b2e56b3f6d99d3b6043c3335a7
edvaldoljr <edvaldoluizj@gmail.com>
2024-06-11 12:54:35 -0300

    Atualizando a classe Conta com LÃombok para metodos get, set e contrutor

--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -1,2 +1,48 @@
-package conta.model;public class Conta {
+package conta.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Conta {
+    private int numero;
+    private int agencia;
+    private int tipo;
+    private String titular;
+    private float saldo;
+
+    public boolean sacar(float valor) {
+        if (this.getSaldo() < valor) {
+            System.out.println("Saldo insuficiente");
+        }
+        this.setSaldo(this.getSaldo() - valor);
+        return true;
+    }
+
+    public void depositar(float valor) {
+        this.setSaldo(this.getSaldo() + valor);
+    }
+
+    public void visualizar() {
+        String tipo = "";
+
+        switch (this.tipo) {
+            case 1 -> tipo = "Conta Corrente";
+
+            case 2 -> tipo = "Conta Poupança";
+        }
+
+        System.out.println("\n\n***********************************************************");
+        System.out.println("Dados da Conta:");
+        System.out.println("***********************************************************");
+        System.out.println("Numero da Conta: " + this.numero);
+        System.out.println("Agência: " + this.agencia);
+        System.out.println("Tipo da Conta: " + tipo);
+        System.out.println("Titular: " + this.titular);
+        System.out.println("Saldo: " + this.saldo);
+    }
 }
+
